1ec30424eb69c29a095bd5ca526a07d2943b777f
Gabriella Barreto <gabriella.gomes@avenuecode.com>
2021-11-01 11:38:56 -0300

    subindo primeira parte do projeto

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
+import Cesta from "./src/telas/Cesta";
+import { useFonts,  
+  Montserrat_400Regular, 
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if(!fonteCarregada) {
+    return <View />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
